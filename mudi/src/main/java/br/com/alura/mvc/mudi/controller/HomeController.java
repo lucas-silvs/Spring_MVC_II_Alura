08950c318cb5cb4c2d904eb3d8dcfd7b60b12240
Lucas da Silva Santos <lucas.parkl@outlook.com>
2022-01-26 19:37:52 -0300

    WIP: Spring MVC II - finalização do modulo 2: - criação da pagina do usuario restrita e configuração da home para torna-la publica - redirect após logout para home - uso de paginação e ordenação na consulta de pedidos - uso do cache local com a anotação @Cacheabel na busca a ser cacheada e ativação do cache com @EnableCaching

--- mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -3,6 +3,8 @@ package br.com.alura.mvc.mudi.controller;
 import br.com.alura.mvc.mudi.model.Pedido;
 import br.com.alura.mvc.mudi.model.StatusPedido;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -20,20 +22,16 @@ public class HomeController {
 
     @Autowired
     private PedidoRepository pedidoRepository;
+
     @GetMapping
     public String home(Model model, Principal principal){
+        Sort sort = Sort.by("dataEntrega").ascending();
+
+        PageRequest pageRequest = PageRequest.of(0,10,sort);
         Pedido pedido = new Pedido();
-       List<Pedido> pedidos = pedidoRepository.findAllByUsuario(principal.getName());
+       List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.ENTREGUE, pageRequest);
         model.addAttribute("pedidos", pedidos);
 
-        return "home";
-    }
-    @GetMapping("/{status}")
-    public String porStatus(@PathVariable("status") String status, Model model){
-        Pedido pedido = new Pedido();
-        List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.valueOf(status.toUpperCase()));
-        model.addAttribute("pedidos", pedidos);
-        model.addAttribute("status",status);
         return "home";
     }
 
--- mudi/src/main/java/br/com/alura/mvc/mudi/controller/PedidoRepository.java
+++ mudi/src/main/java/br/com/alura/mvc/mudi/controller/PedidoRepository.java
@@ -2,6 +2,9 @@ package br.com.alura.mvc.mudi.controller;
 
 import br.com.alura.mvc.mudi.model.Pedido;
 import br.com.alura.mvc.mudi.model.StatusPedido;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,8 +15,12 @@ import java.util.List;
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido,Long> {
 
-    List<Pedido> findByStatus(StatusPedido status);
+    @Cacheable("pedidos")
+    List<Pedido> findByStatus(StatusPedido status, Pageable sort);
 
     @Query("SELECT p FROM Pedido p JOIN p.user u where u.username = :username")
     List<Pedido> findAllByUsuario(@Param("username") String username);
+    @Query("SELECT p FROM Pedido p JOIN p.user u where u.username = :username AND p.status = :status")
+    List<Pedido> findByStatusEUsuario(@Param("status") StatusPedido status,
+                                      @Param("username") String username);
 }
